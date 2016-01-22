5c923b1059fbb62109e0d7107ab476d6b8e121b5
Jorge Arias <mail@jorgearias.cl>
2016-01-21 22:24:00 -0300

    Initialized AngularJS application with Yeoman

---
+++ ng/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bitacoraApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bitacoraApp
+ */
+angular.module('bitacoraApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ ng/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bitacoraApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bitacoraApp
+ */
+angular.module('bitacoraApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
