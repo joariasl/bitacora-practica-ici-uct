1a60caeba1fede589f4897bcd4e5fcddb5e5528c
Jorge Arias <mail@jorgearias.cl>
2016-02-10 23:28:14 -0300

    Added identify path of build script for dinamic calls

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env sh
 
+BASEDIR=$(dirname $0)
 out=bitacora.zip
 local=$(pwd)
 
-rm -f $out
+rm -f $BASEDIR/$out
 
-pushd ng/dist
+pushd $BASEDIR/ng/dist
 grunt build
 zip -r $local/$out .
 popd
 
-pushd www
+pushd $BASEDIR/www
 zip -r $local/$out . -x ".gitignore" "composer.json" "*/composer.json" "*/composer.lock"
 popd
